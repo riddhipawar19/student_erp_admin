6d16c77c9cd48dadd51a597c5881c712a4ae7520
riddhipawar19 <riddhipawar9922@gmail.com>
2022-02-05 16:11:13 +0530

    Student Request page added

--- student-erp-admin/src/components/StudentRequests.js
+++ student-erp-admin/src/components/StudentRequests.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from './Sidebar';
 import './StudentRequests.css'
+import axios from 'axios';
 
 function StudentRequests() {
+
+  useEffect(() => {
+    axios.get("http://localhost:9000/get_data")
+    .then(res => {
+      console.log(res.data);
+    })
+  }, []);
+  
   return <div className="student_requests">
       <Sidebar/>
       <h1>Student Requests</h1>
