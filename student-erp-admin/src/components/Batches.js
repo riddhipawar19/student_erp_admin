a8f8539d13cd2cd4c7759ce9533dd055dac08e92
riddhipawar19 <riddhipawar9922@gmail.com>
2022-02-09 14:42:19 +0530

    Modified batch component

--- student-erp-admin/src/components/Batches.js
+++ student-erp-admin/src/components/Batches.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from './Sidebar';
 import './Batches.css'
+import { AiFillEdit } from "react-icons/ai"
+import { CgDetailsMore } from "react-icons/cg"
+import { useState } from 'react';
+import axios from 'axios';
 
 function Batches() {
+
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:9000/admin/get_courses")
+    .then(res => {
+      setCourses(res.data);
+      console.log(res.data);
+    })
+  }, []);
+
   return <div className="batches">
       <Sidebar/>
-      {/* <div className="batch__names">
-        <h1>Java</h1>
-        <div className="seats">20 / 30</div>
-        <p className="details__btn">Details</p>
-        <p className="edit__btn">Edit</p>
-      </div> */}
+
+      <div className="batches__main">
+        {courses.map (course => {
+          return <div className="batch__data">
+          <div className="batch__data__items">
+          <div>{course.course_name}</div>
+          <div>{course.available_seats} / {course.total_seats}</div>
+
+          <div className="control__btns">
+          <div className="btn" id="details__btn">
+            DETAILS
+            <CgDetailsMore/>
+          </div>
+          <div className="btn" id="edit__btn">
+            EDIT
+            <AiFillEdit/>
+          </div>
+          </div>
+          
+          </div>
+          </div>
+        })}
+      </div>
   </div>;
 }
 
--- student-erp-admin/src/components/StudentRequests.js
+++ student-erp-admin/src/components/StudentRequests.js
@@ -2,34 +2,60 @@ import React, { useEffect , useState} from 'react';
 import Sidebar from './Sidebar';
 import './StudentRequests.css'
 import axios from 'axios';
-import RequestData from './RequestData';
 import { BsCheckLg } from "react-icons/bs"
 import { ImCross } from "react-icons/im"
 
 function StudentRequests() {
 
-  const [data, setdata] = useState([]);
+  const [request, setRequest] = useState([]);
+  const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:9000/get_data")
+    axios.get("http://localhost:9000/get_requests")
     .then(res => {
-      setdata(res.data);
+      setRequest(res.data);
       console.log(res.data);
     })
+
+    axios.get("http://localhost:9000/admin/get_courses")
+    .then(res => {
+      setCourses(res.data);
+      console.log(res.data);
+    })
+
   }, []);
+
+  const handleAccept = (selected_course, student_id) =>{
+    {courses.map(course => {
+      if (selected_course === course.course_name) {
+        axios.put("http://localhost:9000/admin/update_seats",
+        {"course_id":course._id, "student_id":student_id}
+        )
+        .then(res => {
+          console.log("updated");
+        })
+        alert(`Added to ${course.course_name} batch!`)
+      }
+      else{
+        console.log("Course is not available..");
+      }
+    })}
+  }
   
   return <>
   <div className='parent'>
   <Sidebar/>
         <div className="req__main">
-        {data.map (data => {
+        {request.length === 0 ? <h2 id="no_req">No requests</h2> : 
+        <div>
+        {request.map (data => {
           return <div className="data">
           <div className="data__items">
           <div>{data.full_name}</div>
           <div>{data.course}</div>
 
           <div className="control__btns">
-          <div className="btn" id="accept__btn">
+          <div className="btn" id="accept__btn" onClick={e => handleAccept(data.course, data._id)}>
             ACCEPT
             <BsCheckLg/>
           </div>
@@ -42,6 +68,8 @@ function StudentRequests() {
           </div>
           </div>
         })}
+        </div>
+        }
       </div>
       </div>
       </>
